8da3382564e47e9e8134bb45eb518718ea53583d
SnowCoder404 <lenschaumarco@protonmail.com>
2024-10-22 18:56:00 +0200

    Add the one emtoy lines rules.

--- eslint.config.js
+++ eslint.config.js
@@ -9,12 +9,12 @@ export default [
       },
       rules: {
         semi: ['error', 'always'],
-        quotes: ['error', 'backtick'],
-        'max-lines-per-function': ['error', { max: 14, skipBlankLines: true, skipComments: true }],
+        quotes: ['error', 'backtick'], // Verwendung von Backticks
+        'max-lines-per-function': ['error', { max: 14, skipBlankLines: true, skipComments: true }], // Maximale Anzahl von Zeilen pro Funktion
+        'max-empty-lines': ['error', 1], // Maximal 1 leere Zeile erlaubt
         indent: ['error', 4],
         'space-before-blocks': ['error', 'always'],
         'keyword-spacing': ['error', { before: true, after: true }]
       }
     }
-  ];
-  
+];
