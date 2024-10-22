d46fe5fbf79ccc0ee4f0172bbbc3e25773bc509f
SnowCoder404 <lenschaumarco@protonmail.com>
2024-10-22 17:38:32 +0200

    First commit.

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let promError = false;
+
+function getPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (promError) {
+                reject("Hat nicht geklappt");
+            }else {
+                resolve("Hat geklappt");
+            }
+        }, 300)
+    })
+}
+
+async function usePromise() {
+    try {
+        let prom = await getPromise();
+        console.log(prom);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
